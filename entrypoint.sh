4f1b14efc97ab7c88aa3cba1437141a65b3b639e
Maksim Kavun <m.kavun@edenlab.com.ua>
2022-02-25 17:38:21 +0300

    Add fix for curl

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,7 +36,7 @@ function slack_notification() {
     icon_url="https://img.icons8.com/ios-filled/50/000000/0-degrees.png"
     ;;
   esac
-  curl -s -X POST -H 'Content-type: application/json' --data '{"icon_url":'"${icon_url}"',"text":"*Tenant*: '"${TENANT}"'\n*Action*: '"$1"'\n'"*Cluster for branch*: $2"'"}' ${INPUT_RMK_SLACK_WEBHOOK}
+  curl -s -X POST -H 'Content-type: application/json' --data '{"username":"Cluster action","icon_url":"'${icon_url}'","text":"*Tenant*: '"${TENANT}"'\n*Action*: '"$1"'\n'"*Cluster for branch*: $2"'"}' ${INPUT_RMK_SLACK_WEBHOOK}
 }
 
 function destroy_clusters_based_on_pattern() {
