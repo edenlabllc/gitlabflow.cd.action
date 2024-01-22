9713b26994624e6446c2a46bca989a0835172e47
Aliaksandr Panasiuk <apanasyuk@edenlab.com.ua>
2024-01-22 14:44:24 +0100

    add output environments to detect an aws region before running rmk

--- entrypoint.sh
+++ entrypoint.sh
@@ -122,6 +122,7 @@ function export_aws_credentials() {
 
 # Define environment by a specific branch name
 function select_environment() {
+  set -x
   if echo "${1}" | grep -i "develop\|staging\|production" &> /dev/null; then
     export_aws_credentials "${1}"
     return 0
@@ -146,6 +147,8 @@ function select_environment() {
     export_aws_credentials staging
     return 0
   fi
+
+  set +x
 
   >&2 echo "ERROR: Environment \"${1}\" not allowed for environment selection."
   return 1
