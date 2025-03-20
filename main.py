628faa4481bc2c029f19b35b86facb1af7a0d804
Aliaksandr Panasiuk <apanasyuk@edenlab.com.ua>
2025-03-20 17:02:35 +0100

    # - test fix

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
 
         repo = Repo(".")
 
-        print(repo.head)
+        print(repo.active_branch.name)
 
         print(f"Current branch: {github_context.ref_name}")
 
